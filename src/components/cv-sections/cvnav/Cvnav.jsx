3446a7c4c826a01ad416433f2e201ca36d15cc90
jncd-v <jncdev@protonmail.com>
2021-06-29 13:01:59 +0200

    fixed error flag for viewCV in cvsection

--- src/components/cv-sections/cvnav/Cvnav.jsx
+++ src/components/cv-sections/cvnav/Cvnav.jsx
@@ -5,34 +5,52 @@ import { checkStorage } from '../../common'
 
 function Cvnav (props) {
     const [flagIncomplete, setFlagIncomplete] = useState(false);
+    const [completed, setCompleted] = useState(false);
     const [wasFlagged, setWasFlagged] = useState(false)
 
     function handleClick() {
-        if (!checkStorage(props.userId + '_avatar') &&
-            !checkStorage(props.userId + '_profile') &&
-            !checkStorage(props.userId + '_work') &&
-            !checkStorage(props.userId + '_education' &&
-            !checkStorage(props.userId + '_skills') &&
-            !checkStorage(props.userId + '_contact')
-        )) {
+        if (!checkStorage(props.userId + '_avatar') ||
+            !checkStorage(props.userId + '_profile') ||
+            (!checkStorage(props.userId + '_work') || localStorage.getObject(props.userId + '_work').length < 1) ||
+            (!checkStorage(props.userId + '_education') || localStorage.getObject(props.userId + '_education').length < 1) ||
+            (!checkStorage(props.userId + '_skills')  || localStorage.getObject(props.userId + '_education').length < 1) ||
+            (!checkStorage(props.userId + '_contact')  || localStorage.getObject(props.userId + '_contact')[0].tel == 'telephone' )
+        ) {
             setFlagIncomplete(true)
+            console.log('sup')
         }
     }
     function resetFlag() {
         setFlagIncomplete(false)
-        setWasFlagged(false)
+        //setWasFlagged(false)
     }
+    function checkComplete() {
+        if (!checkStorage(props.userId + '_avatar') ||
+            !checkStorage(props.userId + '_profile') ||
+            (!checkStorage(props.userId + '_work') || localStorage.getObject(props.userId + '_work').length < 1) ||
+            (!checkStorage(props.userId + '_education') || localStorage.getObject(props.userId + '_education').length < 1) ||
+            (!checkStorage(props.userId + '_skills')  || localStorage.getObject(props.userId + '_education').length < 1) ||
+            (!checkStorage(props.userId + '_contact')  || localStorage.getObject(props.userId + '_contact')[0].tel == 'telephone' )
+        ) {
+            setCompleted(false)
+            resetFlag()
+        } else {
+            setCompleted(true)
+            resetFlag()
+        }
+    }
+    console.log(completed)
     return (
         <section className="cvnav">
             <h6>sections</h6>
-            <Link to="/create/photo" className='cvnavlinks' onClick={resetFlag}>photo</Link>
-            <Link to="/create/profile" className='cvnavlinks' onClick={resetFlag}>profile</Link>
-            <Link to="/create/work-experience" className='cvnavlinks' onClick={resetFlag}>work experience</Link>   
-            <Link to="/create/education" className='cvnavlinks' onClick={resetFlag}>education</Link>
-            <Link to="/create/skills" className='cvnavlinks' onClick={resetFlag}>skills</Link>
-            <Link to="/create/contact" className='cvnavlinks' onClick={resetFlag}>contact</Link>
-            <Link to={props.complete ? '/create/view' : '#'} className="cvnavlinks" onClick={handleClick}><h3>view CV</h3></Link>
-            <h4 className="section-incomplete">{flagIncomplete && !wasFlagged ? 'complete all the sections!' : ''}</h4>  
+            <Link to="/create/photo" className='cvnavlinks' onClick={checkComplete}>photo</Link>
+            <Link to="/create/profile" className='cvnavlinks' onClick={checkComplete}>profile</Link>
+            <Link to="/create/work-experience" className='cvnavlinks' onClick={checkComplete}>work experience</Link>   
+            <Link to="/create/education" className='cvnavlinks' onClick={checkComplete}>education</Link>
+            <Link to="/create/skills" className='cvnavlinks' onClick={checkComplete}>skills</Link>
+            <Link to="/create/contact" className='cvnavlinks' onClick={checkComplete}>contact</Link>
+            <Link to={completed ? '/create/view' : '#'} className="cvnavlinks" onClick={handleClick}><h3>view CV</h3></Link>
+            <h4 className="section-incomplete">{flagIncomplete ? 'complete all the sections!' : ''}</h4>  
         </section>
     )
 }
--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -7,6 +7,8 @@ function Login (props) {
     const [auth, setAuth] = useState(false)
     const [errorMsg, setErrorMsg] = useState('')
     const [tempUser, setTempUser] = useState(null)
+    const [currEmail, setCurrEmail] = useState('')
+    const [currPassword, setCurrPassword] = useState('')
 
     const passwordRef = useRef(null)
     const emailRef = useRef(null)
@@ -21,7 +23,7 @@ function Login (props) {
         const pw = passwordRef.current.value
         const keys = Object.keys(localStorage).filter( item => item.includes('userInfo'))
         if (keys.length === 0) {
-            setErrorMsg('account does not exist, please sign up')
+            setErrorMsg('no account found for ' + currEmail)
             setAuth(false)
         } else {
             keys.forEach(key => {
@@ -43,7 +45,14 @@ function Login (props) {
         //if no match, say login not found, prompt to create account
         //setUser to null
     }
-
+    function emailHandleChange(e) {
+        const value = e.target.value
+        setCurrEmail(value)
+    }
+    function passwordHandleChange(e) {
+        const value = e.target.value
+        setCurrPassword(value)
+    }
     return (
         <section className="login">
             <section className="section-wrap">
@@ -56,7 +65,8 @@ function Login (props) {
                     ref={emailRef}
                     className="rect-std user"
                     placeholder='email'
-                    maxLength="25"
+                    maxLength="40"
+                    onChange={emailHandleChange}
                 />
 
                 <input
@@ -65,15 +75,20 @@ function Login (props) {
                     ref={passwordRef}
                     className="rect-std user"
                     maxLength="30"
+                    onChange={passwordHandleChange}
                     onBlur={authenticate}
                 />
                 </div>
             </div>
 
                 <section className="login-form-submit">
-                <div className="signup-btn-div">
-                    <Link className="signup-btn" to={auth ? '/create' : '/login'} onClick={auth ? props.setUser(tempUser) : null}>log in</Link>
+                <div className={currEmail !== '' && currPassword !== '' ? 'active-div signup-btn-div' : 'inactive-div signup-btn-div'}>
+                    <Link className={currEmail !== '' && currPassword !== '' ? 'active-div signup-btn' : 'inactive-div signup-btn'} to={auth ? '/create' : '/login'} onClick={auth ? props.setUser(tempUser) : null}>log in</Link>
                 </div>
+                <div className="login-form-error-wrap">
+                    <span className="login-form-error">{errorMsg}</span>
+                </div>
+
                     <div>
                         <p>don't have an account?</p>
                         <Link className="tologin" to={'/signup'} >sign up here</Link>
